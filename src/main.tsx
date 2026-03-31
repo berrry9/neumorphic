569cc668589a06ee5af5a2acccb073174eedd5a2
bettyaaron <bettyaaron6090@gmail.com>
2026-03-31 10:33:08 +0300

    Add Favorites & Navigation

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { FavoritesProvider } from './contexts/FavoritesContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <FavoritesProvider>
+        <App />
+      </FavoritesProvider>
     </ThemeProvider>
   </StrictMode>
 );
