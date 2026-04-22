a0a71a2a8a09a10d896d8d7e820410426f6c5aae
berry9 <155316713+berrry9@users.noreply.github.com>
2026-04-22 10:18:52 +0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ThemeProvider } from './contexts/ThemeContext';
-import { FavoritesProvider } from './contexts/FavoritesContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <FavoritesProvider>
-        <App />
-      </FavoritesProvider>
+      <App />
     </ThemeProvider>
   </StrictMode>
 );
